005d2d3d31a85f1829127c5e96fca1b6d7371646
wgzk11 <91883336+wgzk11@users.noreply.github.com>
2021-10-05 17:20:37 +0800

    修改的第二版本
    修改的第二版本

--- src/main/java/com/openlab/Springboot002Application.java
+++ src/main/java/com/openlab/Springboot002Application.java
@@ -7,7 +7,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 @EnableCaching
 public class Springboot002Application {
-    System.out.println("修改后的代码");
+    System.out.println("修改后的代码第二版本");
     public static void main(String[] args) {
         SpringApplication.run(Springboot002Application.class, args);
     }
