67549b076d1f98c090e8b0db3b92adf394102c01
wgzk11 <91883336+wgzk11@users.noreply.github.com>
2021-10-06 19:32:31 +0800

    更改
    更改

--- src/main/java/com/openlab/Springboot002Application.java
+++ src/main/java/com/openlab/Springboot002Application.java
@@ -9,6 +9,7 @@ import org.springframework.cache.annotation.EnableCaching;
 public class Springboot002Application {
 
     public static void main(String[] args) {
+       System.out.println("aaaaa");
         SpringApplication.run(Springboot002Application.class, args);
     }
 
